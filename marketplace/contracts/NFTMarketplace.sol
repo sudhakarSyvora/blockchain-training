60e6509f9dd3249de0f868017c720032ca92287b
sudhakarbaghel <sudhakarsinghbaghel123321@gmail.com>
2024-03-20 11:56:07 +0530

    Feat: changed variable order

--- marketplace/contracts/NFTMarketplace.sol
+++ marketplace/contracts/NFTMarketplace.sol
@@ -4,30 +4,30 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "hardhat/console.sol";
 
-
 /// @title NFT Marketplace Contract
 /// @notice This contract allows users to buy and sell NFTs (ERC721 and ERC1155 tokens) on a decentralized marketplace.
-contract NFTMarketplace {
+contract NFTMarketplace is Ownable(msg.sender) {
     struct Sale {
         address owner;
         address nftContract;
+        address paymentToken;
         uint256 tokenId;
         uint256 quantity;
         uint256 price;
-        address paymentToken;
     }
+
+    // Address of the marketplace owner who receives fees
+    address public marketplaceOwner;
+
+    // Percentage of fees charged by the marketplace
+    uint256 public feePercentage;
 
     // Mapping to store sale details for each NFT contract and token ID
     mapping(address => mapping(uint256 => Sale)) public sales;
-    
-    // Address of the marketplace owner who receives fees
-    address public marketplaceOwner;
-    
-    // Percentage of fees charged by the marketplace
-    uint256 public feePercentage;
 
     // Events to track sale creation, updates, and completions
     event SaleCreated(
@@ -38,16 +38,9 @@ contract NFTMarketplace {
         uint256 price,
         address paymentToken
     );
-    event SaleUpdated(
-        address owner,
-        uint256 tokenId,
-        uint256 price
-    );
-    event SaleCompleted(
-        address buyer,
-        uint256 tokenId,
-        uint256 price
-    );
+    event SaleUpdated(address owner, uint256 tokenId, uint256 price);
+    event SaleCompleted(address buyer, uint256 tokenId, uint256 price);
+    event FeeUpdated(uint256 _newFee);
 
     /// @notice Constructor function to initialize the marketplace owner and fee percentage
     /// @param _feePercentage The percentage of fees charged by the marketplace
@@ -59,12 +52,26 @@ contract NFTMarketplace {
     /// @notice Modifier to restrict access to only the owner of the NFT
     modifier onlyNFTOwner(address _nftContract, uint256 _tokenId) {
         address tokenOwner;
-        if (IERC721(_nftContract).supportsInterface(0x80ac58cd)) {
+        if (
+            IERC721(_nftContract).supportsInterface(type(IERC721).interfaceId)
+        ) {
             tokenOwner = IERC721(_nftContract).ownerOf(_tokenId);
+        } else if (
+            IERC1155(_nftContract).supportsInterface(type(IERC1155).interfaceId)
+        ) {
+            tokenOwner = IERC1155(_nftContract).balanceOf(
+                msg.sender,
+                _tokenId
+            ) > 0
+                ? msg.sender
+                : address(0);
         } else {
-            tokenOwner = IERC1155(_nftContract).balanceOf(msg.sender, _tokenId) > 0 ? msg.sender : address(0);
+            revert("NFT contract does not support ERC721 or ERC1155 interface");
         }
-        require(tokenOwner == msg.sender, "Only NFT owner can perform this action");
+        require(
+            tokenOwner == msg.sender,
+            "Only NFT owner can perform this action"
+        );
         _;
     }
 
@@ -89,12 +96,19 @@ contract NFTMarketplace {
             sales[_nftContract][_tokenId] = Sale(
                 msg.sender,
                 _nftContract,
+                _paymentToken,
+                _tokenId,
+                _quantity,
+                _price
+            );
+            emit SaleCreated(
+                msg.sender,
+                _nftContract,
                 _tokenId,
                 _quantity,
                 _price,
                 _paymentToken
             );
-            emit SaleCreated(msg.sender, _nftContract, _tokenId, _quantity, _price, _paymentToken);
         } else {
             sales[_nftContract][_tokenId].price = _price;
             emit SaleUpdated(msg.sender, _tokenId, _price);
@@ -115,8 +129,14 @@ contract NFTMarketplace {
             payable(sale.owner).transfer(totalPrice - feeAmount);
         } else {
             IERC20 paymentToken = IERC20(sale.paymentToken);
-            require(paymentToken.allowance(msg.sender, address(this)) >= totalPrice, "Allowance not provided");
-            require(paymentToken.transferFrom(msg.sender, sale.owner, totalPrice), "Payment transfer failed");
+            require(
+                paymentToken.allowance(msg.sender, address(this)) >= totalPrice,
+                "Allowance not provided"
+            );
+            require(
+                paymentToken.transferFrom(msg.sender, sale.owner, totalPrice),
+                "Payment transfer failed"
+            );
         }
 
         payable(marketplaceOwner).transfer(feeAmount);
@@ -125,13 +145,32 @@ contract NFTMarketplace {
         if (excessPayment > 0) {
             payable(msg.sender).transfer(excessPayment);
         }
-        if (IERC721(sale.nftContract).supportsInterface(type(IERC721).interfaceId)) {
-            IERC721(sale.nftContract).safeTransferFrom(sale.owner, msg.sender, _tokenId);
+        if (
+            IERC721(sale.nftContract).supportsInterface(
+                type(IERC721).interfaceId
+            )
+        ) {
+            IERC721(sale.nftContract).safeTransferFrom(
+                sale.owner,
+                msg.sender,
+                _tokenId
+            );
         } else {
-            IERC1155(sale.nftContract).safeTransferFrom(sale.owner, msg.sender, _tokenId, sale.quantity, "");
+            IERC1155(sale.nftContract).safeTransferFrom(
+                sale.owner,
+                msg.sender,
+                _tokenId,
+                sale.quantity,
+                ""
+            );
         }
         sales[_nftContract][_tokenId].owner = msg.sender;
 
         emit SaleCompleted(msg.sender, _tokenId, totalPrice);
+    }
+
+    function setFee(uint256 _newFee) external onlyOwner {
+        feePercentage = _newFee;
+        emit FeeUpdated(_newFee);
     }
 }
